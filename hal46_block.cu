f0c94ca5669615bfae47ec29a5d95aa931eef06d
Alinthok <32737136+Alinthok@users.noreply.github.com>
2024-05-14 14:39:19 +0700

    Add files via upload

---
+++ hal46_block.cu
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cuda_runtime.h>
+
+__global__ void kernel( int *a )
+{
+    int idx = blockIdx.x*blockDim.x + threadIdx.x;
+    a[idx] = blockIdx.x;
+}
+
+int main() {
+    int N = 15;
+    int *a, *h_a;
+
+    cudaMalloc(&a, N * sizeof(int));
+    h_a = (int *)malloc(N * sizeof(int));
+
+    kernel<<<3, 5>>>(a);
+
+    cudaMemcpy(h_a, a, N * sizeof(int), cudaMemcpyDeviceToHost);
+
+    for (int ii = 0; ii < N; ++ii) {
+        printf("%4d ", h_a[ii]);
+    }
+
+    return 0;
+}
